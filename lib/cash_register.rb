566a684fef76c567f8e70ab8a404e780750c169d
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-09-23 18:10:55 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,8 @@
+class CashRegister
+  attr_accessor :total, :last_transaction_amount, :items, :discount
 
+  def initialize(discount = 0)
+    self.total = 0
+    self.discount = discount
+  end
+end
